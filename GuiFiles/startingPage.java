265fe4d89177b5cdb04589ddfab4e0068e922aa0
manvitha-1212 <67196221+manvitha-1212@users.noreply.github.com>
2021-07-22 00:23:32 +0530

    Update startingPage.java

--- GuiFiles/startingPage.java
+++ GuiFiles/startingPage.java
@@ -1,66 +1,57 @@
-package GuiFiles;
-import java.awt.*;
+package window;
+
+
+
+import java.awt.*; 
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;   
 
-public class startingPage extends JPanel {
-    private JLabel applicationName;
+public class Prgm2 extends JPanel {
+    private JLabel jcomp1;
     private JLabel jcomp2;
     private JLabel jcomp3;
-    private JLabel jcomp4;
-    private JButton jcomp5;
-    private JLabel jcomp6;
-    private JTextArea jcomp7;
-    private JLabel jcomp8;
-    private JPasswordField jcomp9;
-    private JLabel jcomp10;
-    private JComboBox Time;
-    private JComboBox jcomp12;
-    private JComboBox jcomp13;
-    private JLabel jcomp14;
-    private JLabel jcomp15;
-    private JComboBox jcomp16;
-    private JLabel jcomp17;
+    private JTextField jcomp4;
+    private JLabel jcomp5;
+    private JTextField jcomp6;
+    private JLabel jcomp7;
+    private JComboBox jcomp8;
+    private JButton jcomp9;
+   
 
-    public startingPage() {
+    public Prgm2() {
+    	
+    	
+       
+    	
         //construct preComponents
-        String[] TimeItems = {"9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 PM"};
-        String[] jcomp12Items = {"00 min", "15 min", "30 min", "45 min", "50 min"};
-        String[] jcomp13Items = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-        String[] jcomp16Items = {"00", "15 ", "30", "45", "50"};
+        String[] jcomp8Items = {"MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"};
 
         //construct components
-        applicationName = new JLabel ("Calendar");
-        jcomp2 = new JLabel ("Enter the following Details...");
-        jcomp3 = new JLabel ("Starting Time : ");
-        jcomp4 = new JLabel ("Total Duration : ");
-        jcomp5 = new JButton ("Continue");
-        jcomp6 = new JLabel ("Name : ");
-        jcomp7 = new JTextArea (5, 5);
-        jcomp8 = new JLabel ("Password : ");
-        jcomp9 = new JPasswordField (5);
-        jcomp10 = new JLabel (":");
-        Time = new JComboBox (TimeItems);
-        jcomp12 = new JComboBox (jcomp12Items);
-        jcomp13 = new JComboBox (jcomp13Items);
-        jcomp14 = new JLabel ("Hour(s)");
-        jcomp15 = new JLabel (":");
-        jcomp16 = new JComboBox (jcomp16Items);
-        jcomp17 = new JLabel ("Minutes");
+        jcomp1 = new JLabel ("CALENDER");
+        
+        jcomp3 = new JLabel ("STARTING TIME :");
+        jcomp4 = new JTextField (5);
+        jcomp5 = new JLabel ("DURATION :");
+        jcomp6 = new JTextField (5);
+        jcomp7 = new JLabel ("DAY :");
+        jcomp8 = new JComboBox (jcomp8Items);
+        jcomp9 = new JButton ("CONTINUE");
+       
 
         //set components properties
-        jcomp4.setToolTipText ("Total amount of time avaliable in that day");
-        jcomp5.setToolTipText ("Press to continue to the next page");
-        jcomp6.setToolTipText ("Username");
+        jcomp3.setToolTipText ("When you are starting the day");
+        jcomp5.setToolTipText ("For the Entire day");
+        jcomp7.setToolTipText ("Select the day you are working on ");
 
         //adjust size and set layout
-        setPreferredSize (new Dimension (944, 581));
+        setPreferredSize (new Dimension (418, 360));
         setLayout (null);
 
         //add components
-        add (applicationName);
-        add (jcomp2);
+        add (jcomp1);
         add (jcomp3);
         add (jcomp4);
         add (jcomp5);
@@ -68,42 +59,35 @@ public class startingPage extends JPanel {
         add (jcomp7);
         add (jcomp8);
         add (jcomp9);
-        add (jcomp10);
-        add (Time);
-        add (jcomp12);
-        add (jcomp13);
-        add (jcomp14);
-        add (jcomp15);
-        add (jcomp16);
-        add (jcomp17);
+       
 
         //set component bounds (only needed by Absolute Positioning)
-        applicationName.setBounds (0, 0, 75, 30);
-        jcomp2.setBounds (15, 60, 180, 25);
-        jcomp3.setBounds (15, 170, 100, 25);
-        jcomp4.setBounds (15, 210, 95, 25);
-        jcomp5.setBounds (735, 495, 175, 65);
-        jcomp6.setBounds (15, 100, 100, 25);
-        jcomp7.setBounds (110, 105, 120, 20);
-        jcomp8.setBounds (15, 125, 100, 25);
-        jcomp9.setBounds (110, 130, 120, 20);
-        jcomp10.setBounds (170, 170, 15, 25);
-        Time.setBounds (110, 170, 60, 25);
-        jcomp12.setBounds (175, 170, 80, 25);
-        jcomp13.setBounds (110, 210, 50, 25);
-        jcomp14.setBounds (160, 210, 50, 25);
-        jcomp15.setBounds (210, 210, 10, 25);
-        jcomp16.setBounds (220, 210, 45, 25);
-        jcomp17.setBounds (265, 210, 100, 25);
+        jcomp1.setBounds (155, -5, 115, 65);
+        
+        jcomp3.setBounds (55, 150, 110, 25);
+        jcomp4.setBounds (200, 150, 100, 25);
+        jcomp5.setBounds (55, 190, 270, 25);
+        jcomp6.setBounds (200, 190, 100, 25);
+        jcomp7.setBounds (55, 105, 100, 25);
+        jcomp8.setBounds (200, 105, 100, 25);
+        jcomp9.setBounds (300, 320, 100, 25);
+       
     }
+    
 
 
     public static void main (String[] args) {
-        JFrame frame = new JFrame ("Calender");
+        JFrame frame = new JFrame ("MyPanel");
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add (new startingPage());
+        frame.getContentPane().add (new Prgm2 ());
+       
         frame.pack();
         frame.setVisible (true);
-    }
-}
+        
+       
+    }}
+
+
+
+
 
