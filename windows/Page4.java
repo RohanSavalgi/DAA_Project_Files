d056a4f2e8da304c0e2404013beb7d1e3c8e3213
Rohan Sanjay Savalgi <savalgirohan@gmail.com>
2021-07-24 22:30:41 +0530

    up

---
+++ windows/Page4.java
@@ -0,0 +1,68 @@
+package window;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Page4 extends JPanel {
+    private JLabel jcomp1;
+    private JTextField jcomp2;
+    private JLabel jcomp3;
+    private JTextField jcomp4;
+    private JLabel jcomp5;
+    private JComboBox jcomp6;
+    private JButton jcomp7;
+    private JButton jcomp8;
+
+    public Page4() {
+        //construct preComponents
+        String[] jcomp6Items = {"MOST IMPORTANT", "INTERMEDIATE", "LEAST IMPORTANT"};
+
+        //construct components
+        jcomp1 = new JLabel ("NAME OF THE EVENT :");
+        jcomp2 = new JTextField (10);
+        jcomp3 = new JLabel ("DURATION :");
+        jcomp4 = new JTextField (5);
+        jcomp5 = new JLabel ("TYPE OF EVENT :");
+        jcomp6 = new JComboBox (jcomp6Items);
+        jcomp7 = new JButton ("VIEW");
+        jcomp8 = new JButton ("NEXT");
+
+        //set components properties
+        jcomp3.setToolTipText ("Time you want to spend on this event");
+
+        //adjust size and set layout
+        setPreferredSize (new Dimension (750, 450));
+        setLayout (null);
+
+        //add components
+        add (jcomp1);
+        add (jcomp2);
+        add (jcomp3);
+        add (jcomp4);
+        add (jcomp5);
+        add (jcomp6);
+        add (jcomp7);
+        add (jcomp8);
+
+        //set component bounds (only needed by Absolute Positioning)
+        jcomp1.setBounds (155, 80, 155, 25);
+        jcomp2.setBounds (330, 80, 190, 25);
+        jcomp3.setBounds (155, 150, 100, 25);
+        jcomp4.setBounds (330, 150, 100, 25);
+        jcomp5.setBounds (155, 220, 115, 25);
+        jcomp6.setBounds (330, 220, 160, 25);
+        jcomp7.setBounds (405, 325, 100, 25);
+        jcomp8.setBounds (550, 325, 100, 25);
+    }
+
+
+    public static void main (String[] args) {
+        JFrame frame = new JFrame ("MyPanel");
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add (new Page4());
+        frame.pack();
+        frame.setVisible (true);
+    }
+}
---
+++ windows/Prgm4.java
@@ -0,0 +1,41 @@
+package window;
+
+
+import javax.swing.*;
+import java.awt.*;
+class Prgm4 {
+	public static void main(String args[]) {
+		
+		
+		
+		 JFrame frame = new JFrame ("MyPanel");
+	        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+	       
+	        frame.getContentPane().setBackground(Color.BLUE);
+	        frame.pack();
+	        frame.setVisible (true);
+	        
+	        private JLabel jcomp1;
+	        private JLabel jcomp2;
+	        private JLabel jcomp3;
+	        private JTextField jcomp4;
+	        private JLabel jcomp5;
+	        private JTextField jcomp6;
+	        private JLabel jcomp7;
+	        private JComboBox jcomp8;
+	        private JButton jcomp9;
+	       
+	        
+	        
+	        
+	        
+	
+	
+	
+	
+	
+	
+	
+	
+   
+}}
